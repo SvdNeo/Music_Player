72abafbb3f4327f9ad320cee1e2dcae67586a49f
SvdNeo <sanjay.d@neoito.com>
2024-01-17 19:33:53 +0530

    Added functionality to delete a song from the playlist

--- script.js
+++ script.js
@@ -143,6 +143,40 @@ const playSong = (id) => {
     setPlayButtonAccessibleText();
   };
   
+  const deleteSong = (id) => {
+    if (userData?.currentSong?.id === id) {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;
+  
+      pauseSong();
+      setPlayerDisplay();
+    }
+  
+    userData.songs = userData?.songs.filter((song) => song.id !== id);
+    renderSongs(userData?.songs); 
+    highlightCurrentSong(); 
+    setPlayButtonAccessibleText(); 
+  
+    if (userData.songs.length === 0) {
+      const resetButton = document.createElement("button");
+      const resetText = document.createTextNode("Reset Playlist");
+  
+      resetButton.id = "reset";
+      resetButton.ariaLabel = "Reset playlist";
+      resetButton.appendChild(resetText);
+      playlistSongs.appendChild(resetButton);
+  
+      resetButton.addEventListener("click", () => {
+        userData.songs = [...allSongs];
+  
+        renderSongs(userData?.songs); 
+        setPlayButtonAccessibleText();
+        resetButton.remove();
+      });
+    }
+  };
+  
+  
   
   
   
